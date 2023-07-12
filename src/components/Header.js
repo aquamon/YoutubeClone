f5fc192ab54593c3a493ae9774393c34b88e8abe
Monash Chhetri <monash.chhetri22@gmail.com>
2023-07-12 15:20:30 +0530

    Caching the search results

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Hamburger from "../../src/assets/images/hamburger.png";
 import YoutubeLogo from "../../src/assets/images/yout.png";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_AUTOSUGGEST } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
+
 const Header = () => {
   const [searchText, setSearchText] = useState("");
 
   const [suggestions, setSuggestions] = useState([]);
 
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
+
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestion(), 200);
+    const timer = setTimeout(() => {
+      if (searchCache[searchText]) {
+        setSuggestions(searchCache[searchText]);
+      } else {
+        getSearchSuggestion();
+      }
+    }, 200);
 
     return () => {
       clearTimeout(timer);
@@ -22,6 +34,8 @@ const Header = () => {
     const data = await fetch(YOUTUBE_AUTOSUGGEST + searchText);
     const json = await data.json();
     setSuggestions(json[1]);
+
+    dispatch(cacheResults({ [searchText]: json[1] }));
   };
 
   const dispatch = useDispatch();
@@ -54,20 +68,27 @@ const Header = () => {
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="px-5 py-2 border border-gray-400  rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
-        <div className="mt-2 ml-20 p-2 px-5 bg-white rounded-lg w-[37rem] shadow-lg fixed border border-gray-300">
-          <ul>
-            {suggestions.map((suggestion) => (
-              <li key={suggestion} className="shadow-md py-2 hover:bg-gray-100">
-                🔍 {suggestion}
-              </li>
-            ))}
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className="z-10 mt-2 ml-20 p-2 px-5 bg-white rounded-lg w-[37rem] shadow-lg fixed border border-gray-300">
+            <ul>
+              {suggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  className="shadow-md py-2 hover:bg-gray-100"
+                >
+                  🔍 {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
       <div className="col-span-1">
